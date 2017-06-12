4d528a3d61e61c97e9b33c01cd2f9f1da6a0bdfc
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-12 14:59:07 +0700

    generate shopify_app and create route(like the video)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
+  controller :session do
+  	get 'login' => :new, :as => :login
+  	post 'login' => :create, :as => :authenticate
+  	get 'auth/shopify/callback'=> :callback
+  	get 'logout' => destroy , :as => :logout
+  end 
+  root :to => 'home#index'
+  mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end

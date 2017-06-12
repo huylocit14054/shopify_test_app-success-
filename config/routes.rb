4cbfec55adac0b3c7f17566e54d1808847f961d1
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-12 15:41:59 +0700

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   	get 'auth/shopify/callback'=> :callback
   end 
   root :to => 'home#index'
-  mount ShopifyApp::Engine, at: '/testshopifyezgamio2'
+  mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end

99cc3be1ecdf1c606c9471f4383ae3c3b1a54126
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-12 15:10:10 +0700

    fix route 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   
   controller :session do
-  	get 'login' => :new, :as => :login
-  	post 'login' => :create, :as => :authenticate
   	get 'auth/shopify/callback'=> :callback
-  	get 'logout' => :destroy , :as => :logout
+
   end 
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/testshopifyezgamio2'

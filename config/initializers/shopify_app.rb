1a8a89433d5fe436314870ec563d57f32962a38b
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-12 15:18:34 +0700

    fix api

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "<api_key>"
-  config.secret = "<secret>"
+  config.api_key = b50217f2eb7ed33b9683d08e3efeb217
+  config.secret = 513e8209823ef5d4151c77cd5bc20c86
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  controller :session do
+  	get 'login' => :new, :as => :login
+  	post 'login' => :create, :as => :authenticate
+  	get 'auth/shopify/callback'=> :callback
+  	get 'logout' => :destroy , :as => :logout
+  end 
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/testshopifyezgamio2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
